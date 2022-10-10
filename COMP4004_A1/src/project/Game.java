e779bd33e3c23580469a277028d99c1a4c44a62c
huiranqu <huiranqu@cmail.carleton.ca>
2022-10-10 17:05:28 -0400

    FullChest_code
    The code of Full chest

--- COMP4004_A1/src/project/Game.java
+++ COMP4004_A1/src/project/Game.java
@@ -71,4 +71,81 @@ public class Game implements Serializable {
         }
         return sc;
     }
+
+    public boolean isFullchest(int[] dieRoll) {
+        for (int i =0;i<dieRoll.length;i++){
+            if (dieRoll[i] == 6){
+                return false;
+            }
+        }
+        int num_CandD = 0;
+        int getScore = 0;
+        int j = 0;
+        int[] Non_CandD_dice = new int[8];
+        for (int i =0;i<dieRoll.length;i++){
+            if (dieRoll[i] == 1 || dieRoll[i] == 2){
+                getScore++;
+                num_CandD++;
+            }else{
+                Non_CandD_dice[j] = dieRoll[i];
+                j++;
+            }
+        }
+        HashMap<Integer, Integer> dict = new HashMap<Integer, Integer>();
+        for (int i : Non_CandD_dice) {
+            if (dict.containsKey(i)) {
+                dict.put(i, dict.get(i) + 1);
+            } else {
+                dict.put(i, 1);
+            }
+        }
+        Collection<Integer> val = dict.values();
+
+        if(num_CandD == 0){           //need all 8 dice to get score
+            if (val.contains(4)){
+                val.remove(4);
+                if (val.contains(4)){
+                    getScore = 8;
+                }
+            }
+            if ((val.contains(8) )||(val.contains(3) && val.contains(5))){
+                getScore = 8;
+            }
+        }
+        if(num_CandD == 1){           //need all 7 dice to get score
+            if ((val.contains(7) )|| (val.contains(3) && val.contains(4))){
+                getScore = 8;
+            }
+        }
+        if(num_CandD == 2){                    //need all 6 dice to get score.
+            if (val.contains(3)){
+                val.remove(3);
+                if (val.contains(3)){
+                    getScore = 8;
+                }
+            }
+            if ((val.contains(6))){
+                getScore = 8;
+            }
+        }
+        if(num_CandD == 3){                       //need all 5 dice to get score
+            if ((val.contains(5) )){
+                getScore = 8;
+            }
+        }
+        if(num_CandD == 4){                       //need all other 5 dice to get score
+            if ((val.contains(4))){
+                getScore = 8;
+            }
+        }
+        if(num_CandD == 5){                       //need all other 3 dice get score.
+            if ((val.contains(3))){
+                getScore = 8;
+            }
+        }
+        if (getScore == 8){
+            return true;
+        }
+        return false;
+    }
 }
