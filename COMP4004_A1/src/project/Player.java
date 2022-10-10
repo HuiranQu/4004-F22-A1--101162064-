ffbf1bae08fdec13c8860a293d2e500efee886df
huiranqu <huiranqu@cmail.carleton.ca>
2022-10-10 14:04:55 -0400

    client code
    The code of client

--- COMP4004_A1/src/project/Player.java
+++ COMP4004_A1/src/project/Player.java
@@ -23,6 +23,160 @@ public class Player implements Serializable{
     Boolean inTestMode = false;
 
 
+
+
+
+
+    // client
+    private class Client {
+        Socket socket;
+        private ObjectInputStream dIn;
+        private ObjectOutputStream dOut;
+
+        public Client() {
+            try {
+                socket = new Socket("localhost", Config.GAME_SERVER_PORT_NUMBER);
+                dOut = new ObjectOutputStream(socket.getOutputStream());
+                dIn = new ObjectInputStream(socket.getInputStream());
+
+                playerId = dIn.readInt();
+
+                System.out.println("Connected as " + playerId);
+                sendPlayer();
+
+            } catch (IOException ex) {
+                System.out.println("Client failed to open");
+            }
+        }
+
+        public Client(int portId) {
+            try {
+                socket = new Socket("localhost", portId);
+                dOut = new ObjectOutputStream(socket.getOutputStream());
+                dIn = new ObjectInputStream(socket.getInputStream());
+
+                playerId = dIn.readInt();
+
+                System.out.println("Connected as " + playerId);
+                sendPlayer();
+
+            } catch (IOException ex) {
+                System.out.println("Client failed to open");
+            }
+        }
+
+        /*
+         * function to send the score sheet to the server
+         */
+        public void sendPlayer() {
+            try {
+                dOut.writeObject(getPlayer());
+                dOut.flush();
+            } catch (IOException ex) {
+                System.out.println("Player not sent");
+                ex.printStackTrace();
+            }
+        }
+
+        /*
+         * function to send strings
+         */
+        public void sendString(String str) {
+            try {
+                dOut.writeUTF(str);
+                dOut.flush();
+            } catch (IOException ex) {
+                System.out.println("String not sent");
+                ex.printStackTrace();
+            }
+        }
+
+        /*
+         * receive scoresheet
+         */
+        public void sendScores(int[] scores) {
+            try {
+                for (int i = 0; i < scores.length; i++) {
+                    dOut.writeInt(scores[i]);
+                }
+                dOut.flush();
+
+            } catch (IOException e) {
+                System.out.println("Score sheet not received");
+                e.printStackTrace();
+            }
+        }
+
+        /*
+         * receive scores of other players
+         */
+        public Player[] receivePlayer() {
+            Player[] pl = new Player[3];
+            try {
+                Player p = (Player) dIn.readObject();
+                pl[0] = p;
+                p = (Player) dIn.readObject();
+                pl[1] = p;
+                p = (Player) dIn.readObject();
+                pl[2] = p;
+                return pl;
+
+            } catch (IOException e) {
+                System.out.println("Score sheet not received");
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                System.out.println("class not found");
+                e.printStackTrace();
+            }
+            return pl;
+        }
+
+        /*
+         * receive scores of other players
+         */
+        public int[][] receiveScores() {
+            try {
+                int[][] sc = new int[3][15];
+                for (int j = 0; j < 3; j++) {
+                    for (int i = 0; i < 15; i++) {
+                        sc[j][i] = dIn.readInt();
+                    }
+                    System.out.println();
+                }
+
+                return sc;
+            } catch (Exception e) {
+                System.out.println("Score sheet not received");
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        /*
+         * receive scores of other players
+         */
+        public int receiveRoundNo() {
+            try {
+                return dIn.readInt();
+
+            } catch (IOException e) {
+                System.out.println("Score sheet not received");
+                e.printStackTrace();
+            }
+            return 0;
+        }
+
+        void terminate() {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.out.println("Score sheet not received");
+                e.printStackTrace();
+            }
+        }
+
+    }
+
     public static void main(String args[]) {
 
 
