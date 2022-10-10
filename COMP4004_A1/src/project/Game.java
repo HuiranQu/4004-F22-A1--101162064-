d367f65fff253ddd2c4476e279cd69f7cd523738
huiranqu <huiranqu@cmail.carleton.ca>
2022-10-10 11:51:03 -0400

    RollDice_code
    to roll 8 dice in 9 dice(one more for coin and Diamond  card)

--- COMP4004_A1/src/project/Game.java
+++ COMP4004_A1/src/project/Game.java
@@ -5,4 +5,13 @@ import java.util.*;
 
 public class Game implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    public int[] rollDice() {
+        int[] die = new int[9];
+        for (int i = 0; i < 8; i++) {
+            int rand = (int) (Math.random() * 6 + 1);
+            die[i] = rand;
+        }
+        return die;
+    }
 }
