fb9343997127b8b78096f69603004c53fe037353
huiranqu <huiranqu@cmail.carleton.ca>
2022-10-10 13:59:40 -0400

    isset_code
    code of determine the set of dice

--- COMP4004_A1/src/project/Game.java
+++ COMP4004_A1/src/project/Game.java
@@ -31,4 +31,20 @@ public class Game implements Serializable {
         }
         return dieRoll;
     }
+
+    public boolean isOfAKind(int x, int[] dieRoll) {
+        HashMap<Integer, Integer> dict = new HashMap<Integer, Integer>();
+        for (int i : dieRoll) {
+            if (dict.containsKey(i)) {
+                dict.put(i, dict.get(i) + 1);
+            } else {
+                dict.put(i, 1);
+            }
+        }
+        Collection<Integer> val = dict.values();
+        if (Collections.max(val) == x  && Collections.max(val) > 2) {
+            return true;
+        }
+        return false;
+    }
 }
