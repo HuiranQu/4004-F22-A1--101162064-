73bd8c5ef3652a981d5217795bc82703d10296d7
huiranqu <huiranqu@cmail.carleton.ca>
2022-10-25 16:39:14 -0400

    Player_IslandSkull_Test

---
+++ COMP4004_A1/src/test/project/Player_IslandSkull_Test.java
@@ -0,0 +1,45 @@
+package project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Player_IslandSkull_Test {
+    Player[] players = new Player[3];
+    Player pl = new Player("A");
+    Player pl1 = new Player("B");
+    Player pl2 = new Player("C");
+    int[] die = {6,6,6,6,1,2,3,4};
+
+    int[] die2 = {6,6,6,2,3,4,5,5};
+    @Test
+    public void island_test(){
+        players[0] = pl;
+        players[1] = pl1;
+        players[2] = pl2;
+        pl.island(players,die,"Coin");
+        Assertions.assertEquals(-400,pl1.getScore());
+        Assertions.assertEquals(-400,pl2.getScore());
+    }
+
+    @Test
+    public void island_SkullCard_test(){
+        players[0] = pl;
+        players[1] = pl1;
+        players[2] = pl2;
+        pl.island(players,die2,"2Skulls");
+        Assertions.assertEquals(-500,pl1.getScore());
+        Assertions.assertEquals(-500,pl2.getScore());
+    }
+
+    @Test
+    public void island_CapCard_test(){
+        players[0] = pl;
+        players[1] = pl1;
+        players[2] = pl2;
+        pl.island(players,die,"Captain");
+        Assertions.assertEquals(-800,pl1.getScore());
+        Assertions.assertEquals(-800,pl2.getScore());
+    }
+}
