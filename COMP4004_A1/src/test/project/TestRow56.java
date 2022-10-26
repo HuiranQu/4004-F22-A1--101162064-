81e0476384017ea1ea41062fdea9095c5ec2501e
huiranqu <huiranqu@cmail.carleton.ca>
2022-10-26 14:42:26 -0400

    TestRow56

---
+++ COMP4004_A1/src/test/project/TestRow56.java
@@ -0,0 +1,37 @@
+package project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestRow56 {
+    Player pl = new Player("a");
+    Game game = new Game();
+    List<String> Fortune = new ArrayList<>(
+            List.of("Chest","Chest","Chest","Chest","Sorceress","Sorceress","Sorceress","Sorceress","Captain","Captain"
+                    ,"Captain","Captain","Monkey&Parrot","Monkey&Parrot","Monkey&Parrot","Monkey&Parrot","Diamond","Diamond","Diamond"
+                    ,"Diamond","Coin","Coin","Coin","Coin","2Skulls","2Skulls","1Skull","1Skull","1Skull","2Swords(300pts)","2Swords(300pts)"
+                    ,"3Swords(500 pts)","3Swords(500pts)","4Swords(1000 pts)","4Swords(1000 pts)"));
+    String Card = game.getFortune();
+    int[] die = game.rollDice();
+
+    @Test
+    public void Row56 (){
+        Card = "Diamond";
+        die[0] = 1;
+        die[1] = 1;
+        die[2] = 1;
+        die[3] = 1;
+        die[4] = 6;
+        die[5] = 6;
+        die[6] = 3;
+        die[7] = 5;
+        pl.scoreRound(0,die,Card);
+        int score = pl.getScore();
+        Assertions.assertEquals(700,score);
+    }
+}
