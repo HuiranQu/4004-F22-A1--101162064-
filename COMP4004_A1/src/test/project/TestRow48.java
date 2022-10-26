9ef3f32b399aff62c2d4b388d19dfa17fafa3b4c
huiranqu <huiranqu@cmail.carleton.ca>
2022-10-26 13:27:40 -0400

    TestRow48

---
+++ COMP4004_A1/src/test/project/TestRow48.java
@@ -0,0 +1,46 @@
+package project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestRow48 {
+    Player pl = new Player("a");
+    Game game = new Game();
+    List<String> Fortune = new ArrayList<>(
+            List.of("Chest","Chest","Chest","Chest","Sorceress","Sorceress","Sorceress","Sorceress","Captain","Captain"
+                    ,"Captain","Captain","Monkey&Parrot","Monkey&Parrot","Monkey&Parrot","Monkey&Parrot","Diamond","Diamond","Diamond"
+                    ,"Diamond","Coin","Coin","Coin","Coin","2Skulls","2Skulls","1Skull","1Skull","1Skull","2Swords(300pts)","2Swords(300pts)"
+                    ,"3Swords(500 pts)","3Swords(500pts)","4Swords(1000 pts)","4Swords(1000 pts)"));
+    String Card = game.getFortune();
+    int[] die = game.rollDice();
+
+    @Test
+    public void Row48(){
+        Card = "Coin";
+        die[0] = 6;
+        die[1] = 4;
+        die[2] = 4;
+        die[3] = 4;
+        die[4] = 4;
+        die[5] = 5;
+        die[6] = 5;
+        die[7] = 5;
+        String[] reroll = {"5","6","7"};
+        int[] re =game.reRollKeep(die,reroll);
+        die[5] = 6;
+        die[6] = 3;
+        die[7] = 3;
+        String[] reroll2 = {"5","6","7"};
+        int[] re2 =game.reRollKeep(die,reroll);
+        die[6] = 6;
+        die[7] = 3;
+        pl.scoreRound(0,re2,Card);
+        int score = pl.getScore();
+        Assertions.assertEquals(0,score);
+    }
+}
