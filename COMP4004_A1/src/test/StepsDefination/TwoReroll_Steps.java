1672f53575de65bd23c82a9121dfdf2a74512fac
huiranqu <huiranqu@cmail.carleton.ca>
2022-11-12 16:05:15 -0500

    TwoReroll_StepsDefination

---
+++ COMP4004_A1/src/test/StepsDefination/TwoReroll_Steps.java
@@ -0,0 +1,25 @@
+package StepsDefination;
+
+import project.Game;
+import project.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoReroll_Steps {
+    Player pl = new Player("a");
+    Game game = new Game();
+    List<String> Fortune = new ArrayList<>(
+            List.of("Chest","Chest","Chest","Chest","Sorceress","Sorceress","Sorceress","Sorceress","Captain","Captain"
+                    ,"Captain","Captain","Monkey&Parrot","Monkey&Parrot","Monkey&Parrot","Monkey&Parrot","Diamond","Diamond","Diamond"
+                    ,"Diamond","Coin","Coin","Coin","Coin","2Skulls","2Skulls","1Skull","1Skull","1Skull","2Swords(300pts)","2Swords(300pts)"
+                    ,"3Swords(500 pts)","3Swords(500pts)","4Swords(1000 pts)","4Swords(1000 pts)"));
+    String Card = game.getFortune();
+    int[] die = game.rollDice();
+    int[] re1 = new int[9];
+    String[] reroll1 = new String[9];
+    int[] re2 = new int[9];
+    String[] reroll2 = new String[9];
+
+
+}
